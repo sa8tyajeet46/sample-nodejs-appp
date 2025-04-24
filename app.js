ee31fa9954ea1a161a7be7a4d55536f8c26c423c
sa8tyajeet46 <satyajeettripathy677@gmail.com>
2025-04-24 23:45:05 +0530

    deploy action test

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app=express();
 
 app.get("/",(req,res)=>{
     return res.status(200).json({
-        message:"hello world"
-    })
+      message: "hello world v2",
+    });
 })
 
 module.exports=app;
